5582d011221396ad4d1953fe861f8ae8fb0c42b3
Blas Oliva <blasoliva@gmail.com>
2015-01-08 10:20:58 -0300

    feed_refresh  func

--- rbot.py
+++ rbot.py
@@ -2,10 +2,12 @@
 """rBot (reebot) sends information to IRC channel"""
 
 import irclib
+irclib.DEBUG = True
 import feedparser
 import sys
 import threading
 import time
+import json
 from config import *
 
 def main():
@@ -15,6 +17,8 @@ def main():
         print( "Error: %s" % e )
         exit()
         
+    c.feed_refresh()
+        
 class rBot:
     def __init__(self):
         self.irc = irclib.IRC()
@@ -23,25 +27,48 @@ class rBot:
  
         for c in CHANNEL_LIST:
             self.server.join( c )
-            self.server.privmsg( c, "Che, I am here" ) # Welcome message
-
-        self.irc.process_forever()
+            self.sendmessage( c, "Che, I am here!" )
         
-    def getsources(self):
         self.feed_list = FEED_LIST
+        self.irc.process_forever()
 
-    def sendmsgqueue(self, message):    
-        # self.msgqueue = []
-        # self.msgqueue.append(message)
-        # self.msg = msgqueue.pop()
-
-        # for c in self.channel_list:
-        #      self.server.privmsg( c, self.msg )
-        # self.irc.process_once()
-        pass
+    def sendmessage(self, channel, message):
+        self.server.privmsg( channel, message )
         
-    def feed_refresh():
-        pass
+    def feed_refresh(self):
+        
+        old_feeds = []
+        new_feeds = []
+        msgqueue = []
+        
+        with open( 'feeds.log', 'r' ) as f:
+            old_feeds = [ line.strip() for line in f ]
+            
+        for feed in self.feed_list:
+            name, source = feed.split( "|" )
+            d = feedparser.parse( source )
+            
+            for entry in d.entries:
+                link = [ entry.link.encode('utf-8') ]
+                if link[0] not in old_feeds:
+                    msgqueue.append( name
+                        + " | " + d.feed.title.encode('utf-8')
+                        + " > " + entry.title.encode('utf-8')
+                        + " : " + entry.link.encode('utf-8') )
+                    new_feeds.append(link[0])
+                    
+        nf = open("feeds.log", "a")
+        for item in new_feeds:
+            nf.write("%s\n" % item)
+        nf.close()
+        
+        while len( msgqueue ) > 0:
+            msgq = msgqueue.pop()
+            for c in CHANNEL_LIST:
+                self.sendmessage( c, msgq )
+
+            # time.sleep(5)
+        threading.Timer( 30, feed_refresh ).start()
             
 if __name__ == "__main__":
     main()
