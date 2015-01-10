99bd1e113cd7e5b1a229184976572ae287902dbf
Blas Oliva <blasoliva@gmail.com>
2015-01-10 15:49:10 -0300

    Bot working almost well

--- rbot.py
+++ rbot.py
@@ -1,47 +1,45 @@
 #!/usr/bin/python
-"""rBot (reebot) sends information to IRC channel"""
+"""rBot (r33bot) sends information to IRC channel"""
 
 import irclib
-irclib.DEBUG = True
+# irclib.DEBUG = True
 import feedparser
 import sys
 import threading
 import time
-import json
 from config import *
 
 def main():
     try:
         c = rBot()
     except irclib.ServerConnectionError, e:
-        print( "Error: %s" % e )
         exit()
-        
-    c.feed_refresh()
-        
+
 class rBot:
     def __init__(self):
+        
         self.irc = irclib.IRC()
         self.server = self.irc.server()
         self.server.connect( IRC_SERVER, IRC_PORT, NICK )
- 
+
         for c in CHANNEL_LIST:
             self.server.join( c )
-            self.sendmessage( c, "Che, I am here!" )
-        
-        self.feed_list = FEED_LIST
-        self.irc.process_forever()
+            self.sendmessage( c, WELCOME_MSG )
+
+         self.feed_list = FEED_LIST
+         self.feed_refresh()
+         self.irc.process_forever()
 
     def sendmessage(self, channel, message):
         self.server.privmsg( channel, message )
-        
+
     def feed_refresh(self):
-        
+
         old_feeds = []
         new_feeds = []
         msgqueue = []
         
-        with open( 'feeds.log', 'r' ) as f:
+        with open( LOG_PATH, 'r' ) as f:
             old_feeds = [ line.strip() for line in f ]
             
         for feed in self.feed_list:
@@ -57,18 +55,18 @@ class rBot:
                         + " : " + entry.link.encode('utf-8') )
                     new_feeds.append(link[0])
                     
-        nf = open("feeds.log", "a")
+        nf = open( LOG_PATH, "a" )
         for item in new_feeds:
-            nf.write("%s\n" % item)
+            nf.write( "%s\n" % item )
         nf.close()
-        
+
         while len( msgqueue ) > 0:
             msgq = msgqueue.pop()
             for c in CHANNEL_LIST:
                 self.sendmessage( c, msgq )
-
-            # time.sleep(5)
-        threading.Timer( 30, feed_refresh ).start()
             
+        time.sleep(5)
+        threading.Timer( 30.0, self.feed_refresh() ).start()
+        
 if __name__ == "__main__":
     main()
