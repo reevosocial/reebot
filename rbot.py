4abea2eb9d5df753e5a89302776e878c16acb02b
Blas Oliva <blasoliva@gmail.com>
2015-01-17 15:19:23 -0300

    feed_refresh func

--- rbot.py
+++ rbot.py
@@ -13,6 +13,7 @@ from config import *
 def main():
     try:
         c = rBot()
+        c.feed_refresh()
     except irclib.ServerConnectionError, e:
         exit()
     
@@ -78,40 +79,31 @@ class rBot:
         response = os.system( "ping -c 1 " + host )
         return response
 
-# def feed_refresh():
+    def feed_refresh(self):
 
-#     old_feeds = []
-#     new_feeds = []
-#     msgqueue = []
+        msgqueue = []
         
-#     with open( LOG_PATH, 'r' ) as f:
-#         old_feeds = [ line.strip() for line in f ]
-            
-#     for feed in FEED_LIST:
-#         name, source = feed.split( "|" )
-#         d = feedparser.parse( source )
-            
-#         for entry in d.entries:
-#             link = [ entry.link.encode('utf-8') ]
-#             if link[0] not in old_feeds:
-#                 msgqueue.append( name
-#                     + " | " + d.feed.title.encode('utf-8')
-#                     + " > " + entry.title.encode('utf-8')
-#                     + " : " + entry.link.encode('utf-8') )
-#                 new_feeds.append(link[0])
-                    
-#     nf = open( LOG_PATH, "a" )
-#     for item in new_feeds:
-#         nf.write( "%s\n" % item )
-#     nf.close()
+        for feed_url in FEED_LIST:
+            name, source = feed_url.split( '|' )
+            f.feedparser.parse( source )
 
-#     while len( msgqueue ) > 0:
-#         msgq = msgqueue.pop()
-#         for channel in CHANNEL_LIST:
-#             c.sendmessage( channel, msgq )
-            
-#     time.sleep(5)
-#     threading.Timer( 15.0, feed_refresh() ).start()
-        
+            feeds = feedparser.parse( source, etag=f.etag )
+
+            if int(feeds.status) != 304:
+                # print "%s" % feeds.debug_message
+                for feed in feeds.entries:
+                    msgqueue.append( name
+                        + " | " + d.feed.title.encode('utf-8')
+                        + " > " + entry.title.encode('utf-8')
+                        + " : " + entry.link.encode('utf-8') )
+
+        while len( msgqueue ) > 0:
+            msgq = msgqueue.pop()
+            for channel in CHANNEL_LIST:
+                c.sendmessage( channel, msgq )
+                time.sleep(3)
+                
+        threading.Timer( 15.0, feed_refresh() ).start()
+
 if __name__ == "__main__":
     main()
