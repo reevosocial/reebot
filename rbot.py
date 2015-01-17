69415863a6cf5e60b0a8f60806e9f90a5977de11
Blas Oliva <blasoliva@gmail.com>
2015-01-17 17:06:54 -0300

    feed_refresh func

--- rbot.py
+++ rbot.py
@@ -13,8 +13,7 @@ from config import *
 def main():
     try:
         c = rBot()
-        c.feed_refresh()
-    except irclib.ServerConnectionError, e:
+     except irclib.ServerConnectionError, e:
         exit()
     
 class rBot:
@@ -31,7 +30,7 @@ class rBot:
             self.sendmessage( channel, WELCOME_MSG )
 
         # Register handlers
-        self.irc.add_global_handler( 'ping', self._ping_ponger, -42 )
+        self.irc.add_global_handler( 'ping', self.ponger, -42 )
         self.irc.add_global_handler( 'privmsg', self.handleprivmessage )
         self.irc.add_global_handler( 'pubmsg', self.handlepubmessage )
 
@@ -42,7 +41,7 @@ class rBot:
         """ Send messages function"""
         self.server.privmsg(channel, message)
         
-    def _ping_ponger(self, connection, event):
+    def ponger(self, connection, event):
         """ Send pong command """
         connection.pong(event.target())
 
@@ -71,10 +70,10 @@ class rBot:
         
         if argument.find ( 'hola r33bot' ) == 0:
             self.sendmessage( target, 'hola ' + source )
-        elif argument.find ( 'feed_refresh' ) == 0:
+        else argument.find ( 'feed_refresh' ) == 0:
             self.feed_refresh()
 
-    def pinghost(self, host):
+    def ping(self, host):
         """ Send ping
         host -- IP address or domain
         """
@@ -88,25 +87,24 @@ class rBot:
         for feed_url in FEED_LIST:
             name, source = feed_url.split( '|' )
             f = feedparser.parse( source )
+            f2 = feedparser.parse( source, etag=f.etag )
 
-            feeds = feedparser.parse( source, etag=f.etag )
-
-            if int(feeds.status) != 304:
-                for feed in feeds.entries:
+            if int(f2.status) != 304:
+                for feed in f2.entries:
                     msgqueue.append( name
-                        + " | " + d.feed.title.encode('utf-8')
-                        + " > " + entry.title.encode('utf-8')
-                        + " : " + entry.link.encode('utf-8') )
+                        + " | " + f2.feed.title.encode('utf-8')
+                        + " > " + feed.title.encode('utf-8')
+                        + " : " + feed.link.encode('utf-8') )
             else:
-                msgqueue.append( feeds.debug_message )
+                msgqueue.append( f2.debug_message )
                 
-        while len( msgqueue ) > 0:
-            msgq = msgqueue.pop()
-            for channel in CHANNEL_LIST:
-                self.sendmessage( channel, msgq )
+            while len( msgqueue ) > 0:
+                msgq = msgqueue.pop()
+                for channel in CHANNEL_LIST:
+                    self.sendmessage( channel, msgq )
                 
         time.sleep(3)
-        threading.Timer( 120, self.feed_refresh ).start()
+        # threading.Timer( 60, self.feed_refresh ).start()
 
 if __name__ == "__main__":
     main()
