a6586ba5f75f48399dc4a8efba43d1d4e6a82721
Blas Oliva <blasoliva@gmail.com>
2015-01-18 14:18:35 -0300

    some changes on variables names

--- config.py
+++ config.py
@@ -1,55 +1,48 @@
 #!/usr/bin/python
 
 # IRC server params
-NICK = "r33bot"
-CHANNEL_LIST = [ "#reevo-dev" ]
-IRC_SERVER = "irc.freenode.org"
-IRC_PORT = 6667
-LOG_PATH = "/opt/rbot/feeds.log"
+nickname = "r33bot"
+channels_list = [ "#reevo-dev" ]
+irc_server = "irc.freenode.org"
+irc_port = 6667
+log_path = "/opt/rbot/feeds.log"
 
 # Messages
-WELCOME_MSG = "Che, I am here!"
+welcome_message = "Che, I am here!"
 
 # Feeds URL's
-FEED_LIST = [
+feed_list = [
     "EQUIPO|http://equipo.reevo.org/activity.atom?key=ad1b69ef44e0e9e6006586081b1bac4fe36ae23e&show_changesets=1&show_documents=1&show_files=1&show_issues=1&show_messages=1&show_news=1&show_wiki_edits=1&utf8=1",
-    #"GIT|http://git.reevo.org/cgit-source//atom",
-    #"GIT|http://git.reevo.org/civicrm/reevo-civicrm-tools//atom",
-    #"GIT|http://git.reevo.org/civicrm/reevo-donation-membership-form//atom",
-    # "GIT|http://git.reevo.org/elgg/reevo-elgg-plugins//atom",
-    # "GIT|http://git.reevo.org/ethercalc/ethercalc//atom",
-    # "GIT|http://git.reevo.org/etherpad/ep_email_notifications//atom",
-    # "GIT|http://git.reevo.org/etherpad/ep_push2delete//atom",
-    # "GIT|http://git.reevo.org/etherpad/ep_syntaxhighlighting//atom",
-    # "GIT|http://git.reevo.org/irc/reebot//atom",
-    # "GIT|http://git.reevo.org/irc/reevo-qwebirc//atom",
-    # "GIT|http://git.reevo.org/irc/reevo-webirc//atom",
-    # "GIT|http://git.reevo.org/mediawiki/reevo-mediawiki-plugins//atom",
-    # "GIT|http://git.reevo.org/piwik/reevo-piwik-theme//atom",
-    # "GIT|http://git.reevo.org/redmine/reevo-redmine-plugins//atom",
-    # "GIT|http://git.reevo.org/redmine/reevo-redmine-theme//atom",
-    # "GIT|http://git.reevo.org/reevo/reevo-base/wordpress//atom",
-    # "GIT|http://git.reevo.org/reevo/reevo-core//atom",
-    # "GIT|http://git.reevo.org/reevo/reevo-core/atom/?h=develop",
-    # "GIT|http://git.reevo.org/reevo/reevo-theme//atom",
-    # "GIT|http://git.reevo.org/reevo/reevo-web//atom",
-    # "GIT|http://git.reevo.org/sitios/casa.reevo.org//atom",
-    # "GIT|http://git.reevo.org/tools/banners-editor//atom",
-    # "GIT|http://git.reevo.org/ushahidi/reevo-ushahidi-theme//atom",
-    # "GIT|http://git.reevo.org/wordpress/reevlog-theme//atom",
-    # "GIT|http://git.reevo.org/wordpress/reevo-wp-hacks//atom",
-    # "GIT|http://git.reevo.org/wordpress/reevo-wp-plugins//atom",
-    # "GIT|http://git.reevo.org/wordpress/reevo-wp-receximport//atom",
-    # "MAPA|http://map.reevo.org/feed",
-    # "RED|http://red.beta.reevo.org/activity?view=rss"
+    "GIT|http://git.reevo.org/cgit-source//atom",
+    "GIT|http://git.reevo.org/civicrm/reevo-civicrm-tools//atom",
+    "GIT|http://git.reevo.org/civicrm/reevo-donation-membership-form//atom",
+    "GIT|http://git.reevo.org/elgg/reevo-elgg-plugins//atom",
+    "GIT|http://git.reevo.org/ethercalc/ethercalc//atom",
+    "GIT|http://git.reevo.org/etherpad/ep_email_notifications//atom",
+    "GIT|http://git.reevo.org/etherpad/ep_push2delete//atom",
+    "GIT|http://git.reevo.org/etherpad/ep_syntaxhighlighting//atom",
+    "GIT|http://git.reevo.org/irc/reebot//atom",
+    "GIT|http://git.reevo.org/irc/reevo-qwebirc//atom",
+    "GIT|http://git.reevo.org/irc/reevo-webirc//atom",
+    "GIT|http://git.reevo.org/mediawiki/reevo-mediawiki-plugins//atom",
+    "GIT|http://git.reevo.org/piwik/reevo-piwik-theme//atom",
+    "GIT|http://git.reevo.org/redmine/reevo-redmine-plugins//atom",
+    "GIT|http://git.reevo.org/redmine/reevo-redmine-theme//atom",
+    "GIT|http://git.reevo.org/reevo/reevo-base/wordpress//atom",
+    "GIT|http://git.reevo.org/reevo/reevo-core//atom",
+    "GIT|http://git.reevo.org/reevo/reevo-core/atom/?h=develop",
+    "GIT|http://git.reevo.org/reevo/reevo-theme//atom",
+    "GIT|http://git.reevo.org/reevo/reevo-web//atom",
+    "GIT|http://git.reevo.org/sitios/casa.reevo.org//atom",
+    "GIT|http://git.reevo.org/tools/banners-editor//atom",
+    "GIT|http://git.reevo.org/ushahidi/reevo-ushahidi-theme//atom",
+    "GIT|http://git.reevo.org/wordpress/reevlog-theme//atom",
+    "GIT|http://git.reevo.org/wordpress/reevo-wp-hacks//atom",
+    "GIT|http://git.reevo.org/wordpress/reevo-wp-plugins//atom",
+    "GIT|http://git.reevo.org/wordpress/reevo-wp-receximport//atom",
+    "MAPA|http://map.reevo.org/feed",
+    "RED|http://red.beta.reevo.org/activity?view=rss"
     ]
 
-REEVO_HOSTS = {
+reevo_hosts = {
     "beta" : "37.187.37.53"}
-
-HELP = {
-    "a" : "aaaa",
-    "b" : "bbbb",
-    "c" : "cccc",
-    "d" : "dddd"}
-
